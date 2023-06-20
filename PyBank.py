4ff120a2ac47e188207f34e926276bc58872c6aa
enmebaragesij99 <96048952+enmebaragesij99@users.noreply.github.com>
2023-06-20 20:06:48 +1000

    Add files via upload

---
+++ PyBank.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+budget_data_csv=os.path.join('Resources', 'budget_data.csv')
+#Stating variables
+total_months=0
+pl_total=0
+change_list=[]
+average_change=0
+change_month=[]
+previous_change=0
+change=0
+increase_amount=0
+increase_date=[""]
+decrease_amount=99999
+decrease_date=[""]
+#Open cvs file
+with open(budget_data_csv) as csvfile:
+    csvreader=csv.reader(csvfile)
+    csv_header=next(csvreader)      
+
+
+    for row in csvreader:
+#Total months
+        total_months+=1
+#Net total of profit/losses       
+        pl_total=pl_total+int(row[1])       
+
+#Average change in Profit/Losses over entire period
+        change=float(row[1])-previous_change
+        previous_change=float(row[1])
+        change_list=change_list+[change]
+        change_month=[change_month]+[row[0]]
+
+
+#Greatest increase in profits:date and amount
+
+        if change > increase_amount:
+            increase_amount=round(change)
+            increase_date=row[0]
+
+#Greatest decrease in losses: date and amount
+
+        if change < decrease_amount:
+            decrease_amount=round(change)
+            decrease_date=row[0]
+change_list.pop(0)
+average_change=round(sum(change_list)/len(change_list),2)
+
+
+
+budget_summary=(
+f"Financial Analysis\n"
+
+f"--------------------------------------\n"
+
+f"Total months: {total_months}\n" 
+
+f"Total: ${pl_total}\n" 
+
+f"Average change: $ {average_change}\n"
+
+f"Greatest Increase in Profits: {increase_date}   (${increase_amount})\n"
+
+f"Greatest Decrease in Profits: {decrease_date}   (${decrease_amount})\n")
+
+print(budget_summary)
+
+text_path="pybank_output.txt"
+
+with open(text_path,'w') as f:
+        f.write(budget_summary)
+       
---
+++ PyPoll.py
@@ -0,0 +1,74 @@
+import os
+import csv
+
+election_data_csv=os.path.join('Resources', 'election_data.csv')
+#Stating variables
+cast_total=0
+candidate_options=[]
+total_votes_count=0
+Charles_count=0
+Diana_count=0
+Raymon_count=0
+Charles_percent=0
+Diana_percent=0
+Raymon_percent=0
+#Open csv file
+with open(election_data_csv) as csvfile:
+    csvreader=csv.reader(csvfile)
+    csv_header=next(csvreader)
+
+    for row in csvreader:
+#List of candidates who received votes
+        candidate_name=row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+#Total number of votes cast
+        total_votes_count +=1
+#Total number of votes each candidate won
+        if row[2]=="Charles Casper Stockham":
+            Charles_count +=1
+        elif row[2]=="Diana DeGette":
+            Diana_count +=1
+        elif row[2]=="Raymon Anthony Doane":
+            Raymon_count +=1
+    vote_counter={"Charles Casper Stockham":Charles_count,"Diana DeGette":Diana_count,"Raymon Anthony Doane":Raymon_count}
+ #Total percentage of votes each candidate won
+    Charles_percent=round((Charles_count/total_votes_count)*100,3)
+    Diana_percent=round((Diana_count/total_votes_count)*100,3)
+    Raymon_percent=round((Raymon_count/total_votes_count)*100,3)
+
+#Winner of election baseed on popular vote
+
+Winner=max(vote_counter, key=vote_counter.get)
+    
+election_summary=(
+    f"Election Results\n"
+
+    f"--------------------------------------\n"
+
+    f"Total Votes: {total_votes_count}\n"
+
+    f"--------------------------------------\n"
+
+    f"Charles Casper Stockam: {Charles_percent}%  ({Charles_count})\n"
+
+    f"Diana DeGette: {Diana_percent}%  ({Diana_count})\n"
+
+    f"Raymon Anthony Doane: {Raymon_percent}%  ({Raymon_count})\n"
+
+    f"--------------------------------------\n"
+
+    f"Winner: {Winner}\n"
+
+    f"--------------------------------------\n" )
+
+print(election_summary)
+
+text_path="pypoll_output.txt"
+
+with open(text_path,'w') as f:
+        f.write(election_summary)
+        
+        
+        
+    
